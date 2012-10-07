02c3c603621c736b2d6e89876db8f9b17f5f9f8d
salar2k <salar2k@gmail.com>
2012-10-07 10:12:55 +0330

    .

--- Salar.IndieDateTime/Properties/AssemblyInfo.cs
+++ Salar.IndieDateTime/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("IndieDateTime")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("SalarSoft")]
+[assembly: AssemblyCompany("Salar")]
 [assembly: AssemblyProduct("IndieDateTime")]
-[assembly: AssemblyCopyright("Copyright © SalarSoft 2012")]
+[assembly: AssemblyCopyright("Copyright © Salar 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.*")]
